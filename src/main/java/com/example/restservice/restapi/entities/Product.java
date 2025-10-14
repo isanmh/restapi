0b83dc52972182bd9c08f1a615cf3dff23e87e7b
isanmh <1157050074@student.uinsgd.ac.id>
2025-10-14 10:12:26 +0700

    Add: Entity Product

--- src/main/java/com/example/restservice/restapi/entities/Product.java
+++ src/main/java/com/example/restservice/restapi/entities/Product.java
@@ -1,5 +1,72 @@
 package com.example.restservice.restapi.entities;
 
-public class Product {
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "product_name", length = 100)
+    private String name;
+
+    @Column(name = "product_description", length = 255)
+    private String description;
+
+    private Double price;
+
+    // Constructors
+    public Product() {
+    }
+
+    // arg Construct
+    public Product(Long id, String name, String description, Double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 
 }
