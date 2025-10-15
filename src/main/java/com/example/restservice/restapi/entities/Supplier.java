581d66ad84b9aab3738d92d69b82804de7e036c6
isanmh <1157050074@student.uinsgd.ac.id>
2025-10-15 12:03:56 +0700

    Add: Supplier

---
+++ src/main/java/com/example/restservice/restapi/entities/Supplier.java
@@ -0,0 +1,5 @@
+package com.example.restservice.restapi.entities;
+
+public class Supplier {
+
+}
---
+++ src/main/java/com/example/restservice/restapi/repositories/SupplierRepo.java
@@ -0,0 +1,9 @@
+package com.example.restservice.restapi.repositories;
+
+import java.util.function.Supplier;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SupplierRepo extends JpaRepository<Supplier, Long> {
+
+}
